99da1c321f67df49fbc5797606cfc78e64585ac4
Michael Wyrick <Michael.Wyrick@caci.com>
2022-05-25 13:19:55 -0400

    working now

--- pipetest_test.go
+++ pipetest_test.go
@@ -4,11 +4,19 @@ import (
 	"log"
 
 	"github.com/sterlingdevils/pipelines/pkg/bufferpipe"
+	"github.com/sterlingdevils/pipelines/pkg/containerpipe"
+	"github.com/sterlingdevils/pipelines/pkg/udppipe"
 )
 
 type Node struct {
+	id int
 }
 
+func (n Node) Key() int {
+	return n.id
+}
+
+// bufferpipe
 func Example_checkinchan_buffer() {
 	b1, err := bufferpipe.New[Node](1)
 	if err != nil {
@@ -19,10 +27,63 @@ func Example_checkinchan_buffer() {
 		log.Fatalln(err)
 	}
 
-	c1 := b1.InChan()
-	c2 := b2.InChan()
+	if b1.InChan() != b2.InChan() {
+		log.Fatalln("inchan is not the same channel")
+	}
 
-	if c1 != c2 {
+	// Output:
+}
+
+// containerpipe
+func Example_checkinchan_container() {
+	b1, err := containerpipe.New[int, Node]()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	b2, err := containerpipe.NewWithPipeline[int, Node](b1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if b1.InChan() != b2.InChan() {
+		log.Fatalln("inchan is not the same channel")
+	}
+
+	// Output:
+}
+
+// dirscanpipe
+
+// retrypipe
+// func Example_checkinchan_retry() {
+// 	b1, err := retrypipe.New()
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	b2, err := retrypipe.NewWithPipeline(b1)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+
+// 	if b1.InChan() != b2.InChan() {
+// 		log.Fatalln("inchan is not the same channel")
+// 	}
+
+// 	// Output:
+// }
+
+// udppipe
+func Example_checkinchan_udp() {
+	b1, err := udppipe.New(9876)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	b2, err := udppipe.NewWithPipeline(9999, b1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if b1.InChan() != b2.InChan() {
 		log.Fatalln("inchan is not the same channel")
 	}
 
