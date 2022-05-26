7a439d921ea97961a76d9772cc87a983fabc6438
Michael Wyrick <Michael.Wyrick@caci.com>
2022-05-26 18:11:10 -0400

    nice new logging test

--- pipetest.go
+++ pipetest.go
@@ -3,15 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 
+	"github.com/sterlingdevils/gobase/pkg/serialnum"
 	"github.com/sterlingdevils/pipelines"
+	"github.com/sterlingdevils/pipelines/converterpipe"
+	"github.com/sterlingdevils/pipelines/logpipe"
+	"github.com/sterlingdevils/pipelines/retrypipe"
 	"github.com/sterlingdevils/pipelines/udppipe"
 	"github.com/sterlingdevils/ratelimiterpipe/pkg/ratelimiterpipe"
 )
 
+// func p2r() func(*udppipe.Packet) *retrypipe.RetryThing[uint64, *udppipe.Packet] {
+// 	mysn := serialnum.New()
+// 	return func(i *udppipe.Packet) *retrypipe.RetryThing[uint64, *udppipe.Packet] {
+// 		return retrypipe.NewRetryThing(mysn.Next(), i)
+// 	}
+// }
+
+func r2p(r *retrypipe.RetryThing[uint64, *udppipe.Packet]) *udppipe.Packet {
+	return r.Thing
+}
+
 // Creates a pipeline of UDP pipes connected by a rate limiter pipe
 func createpipeline() pipelines.Pipeliner[*udppipe.Packet] {
+	mysn := serialnum.New()
+
 	// Creates new UDP pipe on port 9876
 	inpipe, err := udppipe.New(9876)
 	if err != nil {
@@ -25,8 +41,20 @@ func createpipeline() pipelines.Pipeliner[*udppipe.Packet] {
 		log.Fatalln(err)
 	}
 
+	l1 := logpipe.NewWithPipeline[*udppipe.Packet](rlpipe)
+
+	p2 := converterpipe.NewWithPipeline[*udppipe.Packet](
+		func(i *udppipe.Packet) *retrypipe.RetryThing[uint64, *udppipe.Packet] {
+			return retrypipe.NewRetryThing(mysn.Next(), i)
+		},
+		l1)
+
+	l2 := logpipe.NewWithPipeline[*retrypipe.RetryThing[uint64, *udppipe.Packet]](p2)
+	r2 := converterpipe.NewWithPipeline[*retrypipe.RetryThing[uint64, *udppipe.Packet]](r2p, l2)
+	l3 := logpipe.NewWithPipeline[*udppipe.Packet](r2)
+
 	// Creates a new outpipe using the ratelimiter pipe on port 9999
-	outpipe, err := udppipe.NewWithPipeline(9999, rlpipe)
+	outpipe, err := udppipe.NewWithPipeline(9999, l3)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -37,10 +65,10 @@ func createpipeline() pipelines.Pipeliner[*udppipe.Packet] {
 func main() {
 	defer log.Println("leaving main")
 
-	fmt.Printf("Creating Pipeline")
+	fmt.Printf("Creating Pipeline\n")
 	p := createpipeline()
 
-	time.Sleep(30 * time.Second)
+	//	time.Sleep(2 * time.Second)
 
 	p.Close()
 }
