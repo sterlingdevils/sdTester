cd9fd7969552aab25649d962f44747f114e383f1
Michael Wyrick <Michael.Wyrick@caci.com>
2022-05-27 14:31:43 -0400

    packatable

--- pipetest.go
+++ pipetest.go
@@ -13,7 +13,7 @@ import (
 )
 
 // Creates a pipeline of UDP pipes connected by a rate limiter pipe
-func createpipeline() pipelines.Pipeliner[*udppipe.Packet] {
+func createpipeline() pipelines.Pipeliner[udppipe.Packetable] {
 	mysn := serialnum.New()
 
 	// Creates new UDP pipe on port 9876
@@ -23,34 +23,34 @@ func createpipeline() pipelines.Pipeliner[*udppipe.Packet] {
 	}
 
 	// Create Logger
-	l1 := logpipe.NewWithPipeline[*udppipe.Packet]("log1", inpipe)
+	l1 := logpipe.NewWithPipeline[udppipe.Packetable]("log1", inpipe)
 
 	// Create Convert pipe to go from Packet to KeyablePacket by inserting a serial number
-	ptokp := converterpipe.NewWithPipeline[*udppipe.Packet](
-		func(i *udppipe.Packet) *udppipe.KeyablePacket {
-			kp := udppipe.KeyablePacket{
-				Addr:      i.Addr,
-				DataSlice: mysn.AddInc(i.DataSlice),
+	ptokp := converterpipe.NewWithPipeline[udppipe.Packetable](
+		func(i udppipe.Packetable) udppipe.Packetable {
+			kp := &udppipe.KeyablePacket{
+				Addr:      i.Address(),
+				DataSlice: mysn.AddInc(i.Data()),
 			}
-			return &kp
+			return kp
 		}, l1)
 
 	// Create Logger
-	l2 := logpipe.NewWithPipeline[*udppipe.KeyablePacket]("kp", ptokp)
+	l2 := logpipe.NewWithPipeline[udppipe.Packetable]("kp", ptokp)
 
 	// Create Convert pipe to go from Keyable Packet to Packet
-	kptop := converterpipe.NewWithPipeline[*udppipe.KeyablePacket](
-		func(i *udppipe.KeyablePacket) *udppipe.Packet {
-			d, _, _ := serialnum.Remove(i.DataSlice)
-			p := udppipe.Packet{
-				Addr:      i.Addr,
+	kptop := converterpipe.NewWithPipeline[udppipe.Packetable](
+		func(i udppipe.Packetable) udppipe.Packetable {
+			d, _, _ := serialnum.Remove(i.Data())
+			p := &udppipe.Packet{
+				Addr:      i.Address(),
 				DataSlice: d,
 			}
-			return &p
+			return p
 		}, l2)
 
 	// Create Logger
-	l3 := logpipe.NewWithPipeline[*udppipe.Packet]("log3", kptop)
+	l3 := logpipe.NewWithPipeline[udppipe.Packetable]("log3", kptop)
 
 	// Creates a new outpipe using the ratelimiter pipe on port 9999
 	outpipe, err := udppipe.NewWithPipeline(9999, l3)
