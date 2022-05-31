8bd2a61e3763e52ff363d89530f937a815019559
Michael Wyrick <Michael.Wyrick@caci.com>
2022-05-31 16:06:35 -0400

    updated example to use converter to go from one channel tyype to another

--- pipetest_test.go
+++ pipetest_test.go
@@ -3,10 +3,14 @@ package main_test
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
 
+	"github.com/sterlingdevils/pipelines"
 	"github.com/sterlingdevils/pipelines/bufferpipe"
 	"github.com/sterlingdevils/pipelines/containerpipe"
+	"github.com/sterlingdevils/pipelines/converterpipe"
+	"github.com/sterlingdevils/pipelines/filedump"
 	"github.com/sterlingdevils/pipelines/logpipe"
 	"github.com/sterlingdevils/pipelines/udppipe"
 )
@@ -73,4 +77,41 @@ func Example_packet() {
 
 	// Output:
 	// 578437695752307201
+}
+
+type DataHolder struct {
+	data []byte
+}
+
+func (d DataHolder) Data() []byte {
+	return d.data
+}
+
+func Example() {
+	logger := logpipe.New[DataHolder]("inlog")
+
+	// Can't Use logger directly as a Dataer,  Use a convert pipe to change the channel
+	conv := converterpipe.NewWithPipeline[DataHolder](
+		func(i DataHolder) pipelines.Dataer {
+			return i
+		}, logger)
+
+	os.Chdir("/tmp")
+	fd := filedump.NewWithPipeline(conv)
+
+	// Send a Packet
+	fd.InChan() <- udppipe.Packet{DataSlice: []byte("Hello, World!")}
+	fd.InChan() <- udppipe.Packet{DataSlice: []byte("Gimme Jimmy")}
+	fd.InChan() <- udppipe.Packet{DataSlice: []byte("See what happens with special characters\nOn this line")}
+
+	fd.InChan() <- DataHolder{data: []byte("This is another type of input")}
+
+	logger.InChan() <- DataHolder{data: []byte("Input to Logger to make sure it flows")}
+
+	time.Sleep(1 * time.Second)
+
+	fd.Close()
+
+	// Output:
+	//
 }
